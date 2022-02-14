b7c9d7290ae91d88c74b63d7b26edcc9f7b2cbf5
Andrew Rivett <veggiefrog@gmail.com>
2022-02-14 13:14:51 -0500

    Updated remove section delete

--- ui/main.js
+++ ui/main.js
@@ -773,7 +773,7 @@ function ciniki_forms_main() {
     this.field.remove = function() {
 //        if( M.confirm('Are you sure you want to remove field?') ) {
         M.confirm('Are you sure you want to remove this field?', 'Delete Field', function(rsp) {
-            M.api.getJSONCb('ciniki.forms.fieldDelete', {'tnid':M.curTenantID, 'field_id':this.field_id}, function(rsp) {
+            M.api.getJSONCb('ciniki.forms.fieldDelete', {'tnid':M.curTenantID, 'field_id':M.ciniki_forms_main.field.field_id}, function(rsp) {
                 if( rsp.stat != 'ok' ) {
                     M.api.err(rsp);
                     return false;
